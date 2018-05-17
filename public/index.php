58e56c56407f15065faa5ec666b0c3cdac6b2b96
admin <levneedscoffee@gmail.com>
2018-05-17 15:26:14 +0300

    исправил ошибку путей

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$string = file_get_contents('/var/www/auth/config.json');
+$string = file_get_contents('../config.json');
 $path = json_decode($string, true)['path'];
 define('PATH', $path);
 
-require_once ("/var/www/auth/vendor/autoload.php");
-require_once ("/var/www/auth/app/controllers/FrontController.php");
+
+require_once (PATH."/vendor/autoload.php");
+require_once (PATH."/app/controllers/FrontController.php");
 
 $obj = new Auth\Controllers\FrontController();
 $obj->start();
